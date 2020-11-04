c59a889327221c9924d403648986f75e6ae1fa69
Matthew Caseres <matthewcaseres@outlook.com>
2020-11-04 11:11:57 -0600

    Failed attempt at full height

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ import Image from "next/image";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div style={{ backgroundColor: "#1E1E1E" }}>
+    <div style={{ backgroundColor: "#1E1E1E", height: "100%" }}>
       <div className="prose prose-lg lg:max-w-4xl m-auto px-2">
         <Component {...pageProps} />
       </div>
--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,6 @@ import styles from "../styles/Home.module.css";
 import Markdown from "../components/mcq/markdown";
 
 
-
 export default function Home() {
   return (
     <div className="prose">
