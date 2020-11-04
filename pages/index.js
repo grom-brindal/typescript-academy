5a570ff588137751816201552846d8f57327d6ef
Matthew Caseres <matthewcaseres@outlook.com>
2020-11-04 11:20:12 -0600

    add index

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,26 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import Markdown from "../components/mcq/markdown";
-
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="prose">
-      <div className={styles.container}>Some day more things</div>;
+      <div className={styles.container}>
+        Completed Articles
+        <ol>
+          <Link href="/WhyTypeScript">
+            <li>
+              <a>Why use TypeScript</a>
+            </li>
+          </Link>
+          <Link href="/JSTSbook/getting-started">
+            <li>
+              <a>Getting started</a>
+            </li>
+          </Link>
+        </ol>
+      </div>
     </div>
   );
 }
